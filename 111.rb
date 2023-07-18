eecd3c965496b36048738a825033be5258ee8f46
ToriKeeling <tori.keeling@gmail.com>
2023-07-17 20:57:01 -0500

    Update 111.rb

--- 111.rb
+++ 111.rb
@@ -1,5 +1,16 @@
-# Question 1
-# Write code to print 3 different numbers to the terminal.
-puts 3
-puts 4 
-puts 5
+# QUESTION 1
+# Write code to print the numbers 4, 30, 12, 983 on separate lines.
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+puts 2 + 2
+puts 4860 - 4830
+puts 4 * 3
+puts 97317 / 99
+# QUESTION 2
+# Write code using addition, subtraction, muiltiplication, or division with numbers that causes the computer to crash.
+#Add a comment with a copy of the error message.
+puts 10107.6923 x 52.0
+# dylanjost@Dylans-MacBook-Pro actualize-exercises % ruby 111.rb
+# 111.rb:12: syntax error, unexpected tIDENTIFIER, expecting end-of-input
+# 10107.6923 x 52.0
+#Add a comment that describes the error message in your own words.
+# on line 11 there was an incorrect symbol entered
