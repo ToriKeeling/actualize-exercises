9311b5397f5c764ec4dcb8f6963153a0803d78db
ToriKeeling <tori.keeling@gmail.com>
2023-07-17 21:41:49 -0500

    Create 112.rb

---
+++ 112.rb
@@ -0,0 +1,29 @@
+# # QUESTION 1
+# Write code to print hellohellohello using string addition.
+puts "hello" + "hello" + "hello"
+# Then write code to print hellohellohello again, this time using string multiplication.
+puts "hello" * 3
+# # QUESTION 2
+# Read the code below and predict which lines will crash.
+#puts 9 + 3 #woot
+#puts "9" + 3 #crash
+#puts 9 + "3" #crash
+#puts "9" + "3" #woot
+#puts 9 - 3 #woot
+#puts "9" - 3 #crash
+#puts 9 - "3" #crash
+#puts 9 * 3 #woot
+#puts "9" * 3 #woot
+#puts 9 * "3" #crash
+#puts "9" * "3" #crash
+#puts 9 / 3 #woot
+#puts "9" / 3 #woot
+#puts 9 / "3" #crash
+#puts "9" / "3" #crash
+# Then run the code and remove the invalid lines one at a time, until only the valid code remains.
+puts 9 + 3 #woot
+puts "9" + "3" #woot
+puts 9 - 3 #woot
+puts 9 * 3 #woot
+puts "9" * 3 #woot
+puts 9 / 3 #woot
