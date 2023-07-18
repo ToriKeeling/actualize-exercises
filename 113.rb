bc2f54f979c11ad137111b5e74abb3c4c8ef3415
ToriKeeling <tori.keeling@gmail.com>
2023-07-18 11:52:56 -0500

    Update 113.rb

--- 113.rb
+++ 113.rb
@@ -1,20 +1,19 @@
 # QUESTION 1
 # Write code to store 3 different numbers in three different variables.
 # Then write the code to print the three variables added together.
-A= 5
-B= 4
-C= 3
-puts A + B + C
+a = 5
+b = 4
+c = 3
+puts a + b  + c 
 # QUESTION 2
 # If you run the code below, it will crash due to invalid variable names. 
 # Find the invalid variable names and delete the special characters 
 # (be sure to leave the special characters in the valid variable names alone).
-Bob7 = 50
+bob7 = 50
 @bob =50
-Bob_ = 50
-$Bob = 50
-puts Bob7
+bob_ = 50
+$bob = 50
+puts bob7
 puts @bob
-puts Bob_
-puts $Bob
-
+puts bob_
+puts $bob
