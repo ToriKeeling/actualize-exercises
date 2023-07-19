aed27d14ad48cebd5e6cccc25f67fb55bc24045b
ToriKeeling <tori.keeling@gmail.com>
2023-07-19 17:05:01 -0500

    Create 131.rb

---
+++ 131.rb
@@ -0,0 +1,17 @@
+# QUESTION 1
+# Write code using a while loop to print the phrase "Around the world" 144 times.
+daftpunk = 1
+while daftpunk < 144
+    puts "Around the world around the wooorld"
+    daftpunk = daftpunk + 1
+end
+
+# QUESTION 2
+# The following code is supposed to print the numbers 1 to 10, but it currently doesn't work. 
+# Find and fix all the mistakes in the code.
+x = 1
+while x <= 10
+    puts x
+    x = x + 1
+  end
+  
