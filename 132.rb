fe1d25bb1c9e0806f8e2354f11bd493067519e14
ToriKeeling <tori.keeling@gmail.com>
2023-07-21 18:43:16 -0500

    Update 132.rb

--- 132.rb
+++ 132.rb
@@ -2,40 +2,37 @@
 # Finish annotating the code below.
 # Then run the code to see if the output matches your annotation.
 word = "hi"           # 1
-index = 0             # 2
+index = 0             # 2 
+
 while index < 2       # 3 6 9
   word = word + word  # 4 7
   index = index + 1   # 5 8
 end
-p word                # 10
+p word                # 
 
 # 1: word is "hi"
 # 2: word is "hi", index is 0
 # 3: (0 is less than 2, continue)
-# 4: word is hihi
+# 4: word is "hihi"
 # 5: index is 1
 # 6: (1 is less than 2, continue)
-# 7: word is hihihihi
+# 7: word is "hihihihi"
 # 8: index is 2
-# 9: (2 is not less than 2, end the loop)
-# 10: print hihihihi
+# 9: (2 is not less than 2, end loop)
+# 10: prints "hihihihi"
 
 # QUESTION 2
 # Finish annotating the code below.
 # Then run the code to see if the output matches your annotation.
 index = 0             # 1
-while index > 2       # 2 5 8
-  puts index          # 3 6 QUESTION-when I run this I thought that '0' and '1' would be printed befor 'Done!', what am I missing around how this functions?
-  index = index + 1   # 4 7
+
+while index > 2       # 2
+  puts index          
+  index = index + 1    
 end 
-puts "Done!"          # 9
+
+puts "Done!"          # 3
 
 # 1: index is 0
-# 2: (0 is less than 2, continue)
-# 3: prints 0
-# 4: index is 1
-# 5: (1 is less than 2, continue)
-# 6: prints 1
-# 7: index is 2
-# 8: (2 is not less than 2, end the loop)
-# 9: prints Done!
+# 2: (0 is not greater than 1, end loop)
+# 3: prints "Done!"
