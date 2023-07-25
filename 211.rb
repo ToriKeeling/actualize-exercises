b8b982592950840e34bad1857d54b054e483e16b
lucerogiron <gironlucero98@gmail.com>
2023-07-25 06:08:37 -0500

    Add excercise solutions

---
+++ 211.rb
@@ -0,0 +1,15 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Write a program that uses a variable to store a number, then prints that number plus 1
+x = 1
+puts x + 10
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#I wasn't, I thought I needed a loop vs the answer above. 
+#Reading comprehenshion got me along with making this more complicated than it needed to be.
