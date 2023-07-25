1823a763564683a9f8d4e966641b7427447c3649
lucerogiron <gironlucero98@gmail.com>
2023-07-25 07:35:47 -0500

    Create 221.rb

---
+++ 221.rb
@@ -0,0 +1,14 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Write a program that uses a variable to store a word, then prints that word with all capital letters
+x = "woot"
+
+puts x.upcase
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#I was not, I did not have ".upcase" memorized.
