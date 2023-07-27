f9e9f43a5eb47f14051df8bb04adeb2c0eae19de
lucerogiron <gironlucero98@gmail.com>
2023-07-26 23:35:39 -0500

    Create 231.rb

---
+++ 231.rb
@@ -0,0 +1,18 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a program that uses variables to store two numbers, 
+# then prints the numbers added together.
+
+a = 1
+b = 2
+
+c = a + b
+puts c
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# yes this was easy for me, I will tackle the next deliberate practice problem on the list
