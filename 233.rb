2527ce8b222be7178d8d6d9833c1d3a314b860f6
lucerogiron <gironlucero98@gmail.com>
2023-07-28 06:12:01 -0500

    Create 233.rb

---
+++ 233.rb
@@ -0,0 +1,37 @@
+# QUESTION 1
+# Rewrite the while loop using a .times loop, then rewrite it again using a .each loop.
+# 
+numbers = [4, 3, 1, 5]
+new_numbers = []
+index = 0
+while index < numbers.length
+  number = numbers[index]
+  new_numbers.push(number + 5)
+  index = index + 1
+end
+pp new_numbers
+
+numbers = [4, 3, 1, 5]
+new_numbers = []
+index = 0
+numbers.length.times do
+    number = numbers[index]
+    new_numbers.push(number + 5)
+    index = index + 1
+end
+pp new_numbers
+
+numbers = [4, 3, 1, 5]
+numbers.each do |number|
+    puts number + 5
+end
+
+# QUESTION 2
+# Fix the errors with the code below.
+
+colors = ["red", "green", "blue"]
+index = 0
+colors.each do |colors|
+  puts colors.upcase
+  index = index + 1
+end
