afea7d8a78c4a5104f519040776197647b075ce0
ToriKeeling <tori.keeling@gmail.com>
2023-07-28 18:21:12 -0500

    Create 251.rb

---
+++ 251.rb
@@ -0,0 +1,15 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+a = "Hey"
+b = " Y'all!"
+puts a.upcase + b.upcase
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I struggled a bit, I tired to use .uppercase instead of .upcase. I will memorize some of these basics
