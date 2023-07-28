82ba5444e9399ad73d2a79f600303415da503993
ToriKeeling <tori.keeling@gmail.com>
2023-07-28 16:36:13 -0500

    Create 241.rb

---
+++ 241.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a program that uses a variable to store a number, 
+# then prints the number times 10.
+
+knife = 1
+puts knife * 10
+
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# it was not easy for me, my reading comprehension was lacking. I need to slow down for next time 
