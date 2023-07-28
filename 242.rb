fed7bade73b38591d4239e4b881f182b22841cc5
ToriKeeling <tori.keeling@gmail.com>
2023-07-28 16:57:46 -0500

    Create 242.rb

---
+++ 242.rb
@@ -0,0 +1,16 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+
+sisters = ["Tori", "Tara", "Tate"]
+sisters.push("ttTtt")
+p sisters
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+
+
+animals = {"Bird" => "a sky rat", "Rat" => "a sewer bird"}
+animals["Cow"] = "a field Moo"
+pp animals
