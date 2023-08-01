ebe1f08fdccb65dc303d516ab8343f90aa02c8fa
ToriKeeling <tori.keeling@gmail.com>
2023-07-31 20:34:53 -0500

    Create 311.rb

---
+++ 311.rb
@@ -0,0 +1,18 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#Write a while loop to print the numbers 1 through 10.
+
+number = 1
+while number <= 10
+    puts number
+    number = number + 1
+end
+
+# I was not, this tells me I need to practice loops more.
