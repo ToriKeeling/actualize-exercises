1ba77767039256d1e7d6b61ae8c09c144968ae30
ToriKeeling <tori.keeling@gmail.com>
2023-08-02 23:07:23 -0500

    Create 321.rb

---
+++ 321.rb
@@ -0,0 +1,21 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# Use a variable to store a number, 
+# then write a condition that prints 0 if the number is equal to 10, 
+# and prints -1 otherwise.
+
+x = 1
+if x == 10
+    puts 0
+else
+    puts -1
+end
+
+# this was easy, but I would like to practice more difficult conditionals
