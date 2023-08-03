d83618694b012edfe757025021cfb489ae6b61d7
ToriKeeling <tori.keeling@gmail.com>
2023-08-03 09:32:27 -0500

    Create 334.rb

---
+++ 334.rb
@@ -0,0 +1,35 @@
+# Step 1: Read the problem below.
+# Step 2: Decompose the problem into smaller testable problems. DO NOT SOLVE THE PROBLEM YET.
+# Step 3: Solve each problem until you get stuck. YOU DO NOT NEED TO SOLVE THE COMPLETE PROBLEM!
+
+# QUESTION 1
+# Have the user enter 10 words, and allow for duplicate words. 
+# After the user is done, the program will tell the user 
+# which word was entered the most frequently.
+# For example, if the user enters: 
+#   apple, banana, orange, pear, apple, pear, apple, squash, apple, pear
+# The program will say: "apple was your most common word"
+# (That's because apple appeared in the user's list 4 times, more than any other word.)
+
+# create 10 varibles for strings with gets.chomp
+# write a while loop to compare each variable to eachother
+
+puts "Tell me 10 words and I'll tell you which word you used the most"
+word1 = gets.chomp
+word2 = gets.chomp
+word3 = gets.chomp
+word4 = gets.chomp
+word5 = gets.chomp
+word6 = gets.chomp
+word7 = gets.chomp
+word8 = gets.chomp
+word9 = gets.chomp
+word10 = gets.chomp
+
+
+index = 0
+while index < 10
+    index = index + 1
+if word1 = word2
+
+end
