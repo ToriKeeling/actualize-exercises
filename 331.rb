84a0490f15eeef13b2f0e0f1bdad4081f1436511
ToriKeeling <tori.keeling@gmail.com>
2023-08-03 07:28:13 -0500

    Create 331.rb

---
+++ 331.rb
@@ -0,0 +1,24 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# Use variables to store two numbers, 
+# then write a condition that prints 100 if either number is greater than 10, 
+# and prints -100 otherwise.
+
+x = 1
+y = 2
+
+if x > 10 || y > 10
+    puts 100
+  else
+    puts -100
+  end
+  
+
+  # this was harder for me, I forgot about "||" 
