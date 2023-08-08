ce18d22f66434653488b0de83864331de0396150
ToriKeeling <tori.keeling@gmail.com>
2023-08-08 11:35:47 -0500

    add

--- 404.rb
+++ 404.rb
@@ -1,3 +1,4 @@
 #https://docs.google.com/document/d/1Zo_M7T2CKyaJrtpB-vXaVKRfFubXDQ86wPxKfUfy8sc/edit?usp=sharing
 
 puts "Testing for laptop fix"
+puts "second test"
---
+++ 411.rb
@@ -0,0 +1,19 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+# Make a hash to store 3 different states and their captitals. 
+# Then add a new state and capital and print the hash to see the
+
+caps = {"Washton" => "Spoons", "Texas" => "Buckies", "KY" => "Franks"}
+caps["USA"] = "delusion"
+pp caps
+
+# at first I created an array with hashes of the info, 
+# I will be working on solidifying the difference between arrays, hashes, arrays with hashes
