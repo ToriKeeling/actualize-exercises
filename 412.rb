b554bc111e9fb9ac8886734b04d42ae9bd023b30
ToriKeeling <tori.keeling@gmail.com>
2023-08-08 11:45:10 -0500

    add

---
+++ 412.rb
@@ -0,0 +1,9 @@
+# QUESTION 1
+# Write a method that takes in an array of numbers and returns its sum.
+# Then run the method and print the result.
+
+numbers = [1, 2, 3]
+def get_sum(numbers)
+    numbers.sum
+end
+pp get_sum(numbers)
