1ed3203a88b5da2b3bf397a5b142cc9533b0e1f1
ToriKeeling <tori.keeling@gmail.com>
2023-08-10 16:18:22 -0500

    Create 441.rb

---
+++ 441.rb
@@ -0,0 +1,19 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# Make a hash to store definitions for 2 different words. 
+# Then add a new word and definition and print the hash to see the result.
+
+words = { cat: "meow", cow: "moo"}
+words[:dog] = "ruff"
+pp words
+
+# I did not get this on thei first try, 
+# I tried to write an array of hashes which is not what it asked
+# plowing ahead with the deliberate practices with arrays and hashes so they can sink into my noggin jelly
